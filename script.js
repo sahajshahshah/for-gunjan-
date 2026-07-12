7d089a267b548de960017f0028b77e52f4973a37
sahajshahshah <sahajshah33@gmail.com>
2026-07-12 11:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-document.getElementById("continueBtn").addEventListener("click", function(){
+document.getElementById("continueBtn").addEventListener("click", function () {
 
 document.querySelector(".container").innerHTML = `
 
-<h1 class="title">
-Hi, Gunjan 🤍
-</h1>
+<h1 class="title" id="typing"></h1>
 
-<p class="subtitle">
-Before you continue...
-
-I just want two minutes of your time.
-
-Nothing more.
-
-Nothing less.
-</p>
-
-<button id="nextBtn">
-Continue →
-</button>
+<p class="subtitle" id="sub"></p>
 
 `;
+
+const title = "Every great story...";
+const subtitle = "starts with a single moment.";
+
+let i = 0;
+
+function typeTitle() {
+    if(i < title.length){
+        document.getElementById("typing").innerHTML += title.charAt(i);
+        i++;
+        setTimeout(typeTitle,60);
+    }else{
+        document.getElementById("sub").innerHTML = subtitle;
+
+        setTimeout(()=>{
+            document.getElementById("sub").innerHTML += "<br><br>.";
+        },500);
+
+        setTimeout(()=>{
+            document.getElementById("sub").innerHTML += ".";
+        },1000);
+
+        setTimeout(()=>{
+            document.getElementById("sub").innerHTML += ".";
+        },1500);
+
+        setTimeout(()=>{
+            alert("Scene 4 coming...");
+        },3000);
+    }
+}
+
+typeTitle();
 
 });
