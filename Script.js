6381a3ffc03c6fe12724384cf97182e7a8c72a87
sahajshahshah <sahajshah33@gmail.com>
2026-07-12 11:11:38 +0530

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+    const button = document.getElementById("continueBtn");
+
+    button.addEventListener("click", () => {
+
+        button.innerHTML = "Loading... 🤍";
+
+        button.style.transform = "scale(0.95)";
+
+        setTimeout(() => {
+
+            alert("🌌 Welcome to Project Sunrise.\n\nOur story begins here...");
+
+            button.innerHTML = "Continue 🤍";
+            button.style.transform = "scale(1)";
+
+        },1000);
+
+    });
+
+});
